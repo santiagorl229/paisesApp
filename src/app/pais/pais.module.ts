3cee9a3bd1a58842ca9e62fbc6211caa101d670a
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2022-04-04 23:35:49 -0500

    Se termina el video 11 del modulo 8

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -4,6 +4,7 @@ import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -20,7 +21,8 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     VerPaisComponent],
     
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class PaisModule { }
