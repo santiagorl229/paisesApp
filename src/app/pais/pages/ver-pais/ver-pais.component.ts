6e3291b8e80f9fe4ee28ec7b63f04cb0d2de09e1
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2022-07-07 07:41:20 -0500

    Empezar el video 22 del modulo 8Spa-paises de la parte 2

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisServices: PaisService 
+  ){}
 
   ngOnInit(): void {
+    this.activatedRoute.params.
+    pipe(
+      switchMap(({id})=>this.paisServices.getPaisPorAlpha(id))
+    )
+    .subscribe(resp=>{console.log(resp)})
+    /*this.activatedRoute.params.subscribe(({id})=>{
+      this.paisServices.getPaisPorAlpha(id).subscribe(pais=>{
+        console.log(pais)
+      })
+    })*/
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -9,6 +9,7 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
+  private apiUrlCity: string ='https://restcountries.com/v2';
 
   constructor(private http: HttpClient) { }
 
@@ -16,5 +17,13 @@ export class PaisService {
     const url= `${this.apiUrl}/name/${termino}`;
     console.log(url);
     return this.http.get<Country[]>(url);
+  }
+  buscarCapital(termino: string):Observable<Country[]>{
+    const apiUrlCity = `${this.apiUrlCity}/capital/${termino}`;
+    return this.http.get<Country[]>(apiUrlCity);
+  }
+  getPaisPorAlpha(id:string):Observable<Country>{
+    const url= `${this.apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
   }
 }
